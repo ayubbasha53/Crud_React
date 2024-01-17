6557e4f334b12ca7c6c659add4ce9b6a35aa058f
Ayub Basha <ayubbasha53@gmail.com>
2024-01-17 15:27:29 +0530

    Crud ReactJS

---
+++ src/EmployeeData.js
@@ -0,0 +1,20 @@
+export const EmployeeData = [
+    {
+        id:1,
+        firstName : 'Mark',
+        lastName : 'David',
+        age:20
+    },
+    {
+        id:2,
+        firstName : 'Virat',
+        lastName : 'Kohli',
+        age:34
+    },
+    {
+        id:3,
+        firstName : 'Rohit',
+        lastName : 'Sharma',
+        age:35
+    },
+]
